981e1ec045d256cd6f53c48403eb612059da172c
Josh <ejosh.manuel@gmail.com>
2019-05-03 00:08:20 -0700

    components initialized; navbar html added;

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Footer from "./components/Footer";
+import Game from "./components/Game";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Score from "./components/Score";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Nav />
+    // <Header />
   );
 }
 
---
+++ src/components/Nav/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-default navbar-fixed-top">
+            <div>Score: 0 </div>
+            <a href="/">Restart</a>
+        </nav>
+
+    );
+}
+
+export default Navbar;
