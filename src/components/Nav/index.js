00191b597e402ae06e4e0be9bedb1483dc7e1449
Josh <ejosh.manuel@gmail.com>
2019-05-05 23:55:11 -0700

    nav instructions updated

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function Navbar() {
     return (
         <nav className="navbar navbar-default navbar-fixed-top">
-            <div className="instructions">"Click every employee to win, but DON'T click them more than once" -Wayne Gretzky" -Michael Scott</div>
+            <div className="instructions">"Click every employee to win, but  <u><font color="#c5bfb5">don't</font></u> click them more than once" -Wayne Gretzky" -Michael Scott</div>
         </nav>
 
     );
